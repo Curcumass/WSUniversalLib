48226a249439d017f37fe04582dd42bba52cdb66
Curcumass <hronos1228@yandex.ru>
2023-03-13 09:35:22 +0300

    first commit

---
+++ ConsoleApp1/Calculation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public  class Calculation
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public int GetQuantityForProduct(int productType, int materialType, int count, float width, float length)
+        {
+
+            if(productType == 0 || materialType == 0 || count == 0 || width <= 0 || length <= 0)
+            {
+                return -1;
+            }
+            float productTypeCoefficient = 0f;
+            float materialScrapRate = 0f; // процент брака материала
+
+            switch (productType)
+            {
+                case 1: productTypeCoefficient = 1.1f;
+                    break;
+                case 2: productTypeCoefficient = 2.5f;
+                    break;
+                case 3: productTypeCoefficient = 8.43f;
+                    break;
+            }
+            if (productTypeCoefficient == 0f)
+            {
+                return -1;
+            }
+            
+            switch (materialType)
+            {
+                case 1: materialScrapRate = 0.3f;
+                    break;
+                case 2: materialScrapRate = 0.12f;
+                    break;
+            }
+            if (materialScrapRate == 0f)
+            {
+                return -1;
+            }
+
+            float square = SquareCalculation(width, length);
+
+            float qualityRawMaterials = QualityRawMaterialsCalculation(square, productTypeCoefficient, count);
+            float materialScrape = (float)Math.Ceiling((qualityRawMaterials * materialScrapRate) / 100);
+
+            int result = Convert.ToInt32(Math.Ceiling(qualityRawMaterials + materialScrape));
+
+            return result;
+        }
+        public float SquareCalculation(float width, float length)
+        {
+            return width * length;
+        }
+
+        public float QualityRawMaterialsCalculation(float square, float productTypeCoefficient, int count)
+        {
+            return square * productTypeCoefficient * count;
+        }
+    }
+}
---
+++ UnitTestProject1/GetQuantityForProductTests.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ConsoleApp1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class GetQuantityForProductTests
+    {
+        [TestMethod]
+        public void ReturnCorrectValue()
+        {
+            int productType = 3;
+            int materialType = 1;
+            int count = 15;
+            float width = 20;
+            float length = 45;
+
+            Calculation calculation = new Calculation();
+            int expected = 114148;
+
+            int actual = calculation.GetQuantityForProduct(productType, materialType, count, width, length);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ProductTypeEqualsZero()
+        {
+            int productType = 0;
+            int materialType = 1;
+            int count = 15;
+            float width = 20;
+            float length = 45;
+
+            Calculation calculation = new Calculation();
+            int expected = -1;
+
+            int actual = calculation.GetQuantityForProduct(productType, materialType, count, width, length);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NonExistentProductType()
+        {
+            int productType = 20;
+            int materialType = 1;
+            int count = 15;
+            float width = 20;
+            float length = 45;
+
+            Calculation calculation = new Calculation();
+            int expected = -1;
+
+            int actual = calculation.GetQuantityForProduct(productType, materialType, count, width, length);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void NonExistentMaterialType()
+        {
+            int productType = 20;
+            int materialType = 0;
+            int count = 15;
+            float width = 20;
+            float length = 45;
+
+            Calculation calculation = new Calculation();
+            int expected = -1;
+
+            int actual = calculation.GetQuantityForProduct(productType, materialType, count, width, length);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void CountEqualZero()
+        {
+            int productType = 20;
+            int materialType = 1;
+            int count = 0;
+            float width = 20;
+            float length = 45;
+
+            Calculation calculation = new Calculation();
+
+            int expected = -1;
+
+            int actual = calculation.GetQuantityForProduct(productType, materialType, count, width, length);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ImpossibleWidth()
+        {
+            int productType = 20;
+            int materialType = 1;
+            int count = 15;
+            float width = -20;
+            float length = 45;
+
+            Calculation calculation = new Calculation();
+            int expected = -1;
+
+            int actual = calculation.GetQuantityForProduct(productType, materialType, count, width, length);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ImpossibleLength()
+        {
+            int productType = 20;
+            int materialType = 1;
+            int count = 15;
+            float width = 20;
+            float length = -12;
+
+            Calculation calculation = new Calculation();
+            int expected = -1;
+
+            int actual = calculation.GetQuantityForProduct(productType, materialType, count, width, length);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    [TestClass]
+    public class SquareCalculationTests
+    {
+        [TestMethod]
+        public void ReturnCorrectValue()
+        {
+            float width = 3f;
+            float length = 4f;
+
+            Calculation calculation = new Calculation();
+
+            float expected = width * length;
+            float actual = calculation.SquareCalculation(width, length);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+
+
+    [TestClass]
+    public class QualityRawMaterialsCalculationTests
+    {
+        [TestMethod]
+        public void ReturnCorrectValue()
+        {
+            Calculation calculation = new Calculation();
+
+            float square = 15f;
+            float coefficient = 1.1f;
+            int count = 3;
+
+            float expected = square * count * coefficient;
+
+            float actual = calculation.QualityRawMaterialsCalculation(square, coefficient, count);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void ImpossibleSquare()
+        {
+            Calculation calculation = new Calculation();
+
+            float square = -1;
+            float coefficient = 1.1f;
+            int count = 3;
+
+            float expected = -1;
+
+            float actual = calculation.QualityRawMaterialsCalculation(square, coefficient, count);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+
+}
